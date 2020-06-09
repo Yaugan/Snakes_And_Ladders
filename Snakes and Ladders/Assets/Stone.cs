28c0b2d1b97f4534560ea29f88be4ff5c416ccc8
Yaugan <yaugansd45@gmail.com>
2020-06-09 16:49:36 +0530

    Added dice and player connection
    error: the player moves twice the number of times when the dice is rolled and reset.

---
+++ Snakes and Ladders/Assets/Stone.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stone : MonoBehaviour
+{
+    private int routePosition;
+    private bool isMoving;
+
+    public int steps;
+    public Route currentRoute;
+    public Dice currentDice;
+
+    public void PlayerMovement()
+    {
+        if(currentDice.rollDice && !isMoving)
+        {
+            steps = currentDice.SideValueCheck(steps);
+            Debug.Log("Dice Rolled " + steps);
+            
+
+            if(routePosition + steps < currentRoute.childNodeList.Count)
+            {
+                StartCoroutine(Move());                
+            }
+            else
+            {
+                Debug.Log("Rolled Number is too high " + steps);
+            }
+        }
+    }
+
+
+    IEnumerator Move()
+    {
+        if(isMoving)
+        {
+            yield break;
+        }
+        isMoving = true;
+
+        while(steps > 0)
+        {
+            Vector3 nextPos = currentRoute.childNodeList[routePosition + 1].position;
+            while(MoveToNextNode(nextPos))
+            {
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(0.1f);
+
+            steps--;
+            routePosition++;
+        }
+
+        isMoving = false;
+    }
+
+    private bool MoveToNextNode(Vector3 goal)
+    {
+        return goal != (transform.position = Vector3.MoveTowards(transform.position, goal, 8f * Time.deltaTime));
+    }
+}
