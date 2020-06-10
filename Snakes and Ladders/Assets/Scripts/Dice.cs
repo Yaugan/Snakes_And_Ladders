ce8d7e19079e988d5b396c1eb062eff748ccc21f
Yaugan <yaugansd45@gmail.com>
2020-06-10 07:48:46 +0530

    connected the whole player and dice based system
    make a turn based game out of it make it for two players so we can send him.

--- Snakes and Ladders/Assets/Scripts/Dice.cs
+++ Snakes and Ladders/Assets/Scripts/Dice.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class Dice : MonoBehaviour
 {
+    
     private Rigidbody rb;
     private bool hasLanded;
     private bool thrown;
@@ -15,81 +16,127 @@ public class Dice : MonoBehaviour
 
     public DiceSide[] diceSides;
 
-     
+    public Stone player;
+
+
+    public float sideValueTime;
+    public float resetTime;
+
+
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         initialPos = transform.position;
         rb.useGravity = false;
+
     }
 
-    public void DiceThrow()
+    public void DiceRoll()
     {
         if(rollDice.interactable == true)
-        {            
-                RollDice();           
-        }
-
-        if (rb.IsSleeping() && !hasLanded && thrown)
         {
-            hasLanded = true;
-            rb.useGravity = false;
-            rb.isKinematic = true;
+            StartCoroutine("DiceThrow");
+        }
+        
+    }
 
-            SideValueCheck(diceValue);
-        }
-        else if (rb.IsSleeping() && hasLanded && diceValue == 0)
-        {
-            RollAgain();
-        }
+    private IEnumerator DiceThrow()
+    {
+        Debug.Log("RollDice Called");
+        RollDice();
+        yield return new WaitForSeconds(sideValueTime);
+        rollDice.gameObject.SetActive(false);
+        SideValueCheck(diceValue);
+        player.movePlayer = true;
+        player.PlayerMovement();
+        StartCoroutine("Reset");
+
+        //if (rb.IsSleeping() && !hasLanded && thrown)
+        //{
+        //    Debug.Log("Side Value is Checked");
+        //    hasLanded = true;
+        //    rb.useGravity = false;
+        //    rb.isKinematic = true;
+
+        //    Debug.Log("This is runned");
+        //    SideValueCheck(diceValue);
+        //}
+        //else if (rb.IsSleeping() && hasLanded && !thrown)
+        //{
+        //    Debug.Log("Roll Again called");
+        //    RollAgain();
+        //}
     }
 
     private void RollDice()
     {
-        
-        if(!thrown && !hasLanded)
+
+        if (!thrown && !hasLanded)
         {
+            Debug.Log("!thrown && !hasLanded if condition");
             thrown = true;
-            rb.useGravity = true;
+            rb.useGravity = true;            
             rb.AddTorque(Random.Range(500, 1000), Random.Range(500, 1000), Random.Range(500, 1000));
+            //SideValueCheck(diceValue);
+            //player.movePlayer = true;
+            //player.PlayerMovement();
+            //StartCoroutine("Reset");
+
         }
-        else if(thrown && hasLanded)
+        else if (thrown && hasLanded)
         {
-            Reset();
+            Debug.Log("Reset Called");
+            StartCoroutine("Reset");
         }
 
     }
 
     private void RollAgain()
     {
-        Reset();
+        Debug.Log("RollAgain Accessed");
+        //Reset();
+        Debug.Log("RollAgain Reset Called");
         thrown = true;
         rb.useGravity = true;
         rb.AddTorque(Random.Range(500, 1000), Random.Range(500, 1000), Random.Range(500, 1000));
     }
 
-    private void Reset()
+    public IEnumerator Reset()
     {
+        yield return new WaitForSeconds(resetTime);
+
+        Debug.Log("Reset Accessed");
         transform.position = initialPos;
         thrown = false;
         hasLanded = false;
         rb.useGravity = false;
         rb.isKinematic = false;
-        
+
+        yield return new WaitForSeconds(.5f);
+        rollDice.gameObject.SetActive(true);
+        //RollAgain();
     }
 
-    public int SideValueCheck(int value)
+
+
+    internal int SideValueCheck(int value)
     {
-        foreach(DiceSide side in diceSides)
+        
+        Debug.Log("Side Value Checked" + value);
+        foreach (DiceSide side in diceSides)
         {
-            if(side.OnGround())
+            if (side.OnGround())
             {
                 value = side.sideValue;
-                Debug.Log(value + " has been rolled!");                
+                Debug.Log(value + " has been rolled!");
             }
         }
+        Debug.Log("value: " + value);
+       
         return value;
     }
+
+
 
 }
